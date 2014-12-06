a65f681abcf9558ae7b3d06ff590af00254dde9a
marshall <marshall@9c77b406-2277-476a-9c5a-3ec351f1475f>
2014-12-06 17:20:48 +0000

    Adding copyFiles.sh to repository

---
+++ trunk/oma2UiIndependent/copyFiles.sh
@@ -0,0 +1,23 @@
+FOLDER=/Users/mblong/oma2/branches/oma2_threaded/oma2
+
+DEST=/Users/mblong/qtoma2/trunk/oma2UiIndependent
+#
+cp $FOLDER/oma2/ImageBitmap.h $DEST/
+cp $FOLDER/ImageBitmap.mm $DEST/ImageBitmap.cpp
+cp $FOLDER/oma2/comdec.h $DEST/
+cp $FOLDER/oma2/comdec.mm $DEST/comdec.cpp
+cp $FOLDER/oma2/commands_1.h $DEST/
+cp $FOLDER/oma2/commands_1.mm $DEST/commands_1.cpp
+cp $FOLDER/oma2/dcrawx.h $DEST/
+cp $FOLDER/oma2/dcrawx.mm $DEST/dcrawx.cpp
+cp $FOLDER/oma2/dofft.h $DEST/
+cp $FOLDER/oma2/dofft.mm $DEST/dofft.cpp
+cp $FOLDER/oma2/gluedCommands.h $DEST/
+cp $FOLDER/oma2/gluedCommands.mm $DEST/gluedCommands.cpp
+cp $FOLDER/oma2/image.h $DEST/
+cp $FOLDER/oma2/image.mm $DEST/image.cpp
+cp $FOLDER/oma2/image_support.h $DEST/
+cp $FOLDER/oma2/image_support.mm $DEST/image_support.cpp
+cp $FOLDER/oma2/oma2.h $DEST/
+cp $FOLDER/oma2/oma2help.txt $DEST/
+
